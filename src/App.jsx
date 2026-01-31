e6b0242d432e3f4021ce5a560a336745ba89d27d
rob <robmathew21@gmail.com>
2026-01-31 09:56:12 -0600

    updating App.jsx which has nav and footer components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { Nav } from "./app/components/Nav";
+import { Hero } from "./app/components/Hero";
 import { About } from "./app/components/About";
 import { Services } from "./app/components/Services";
 import { FAQ } from "./app/components/FAQ";
-import {Contact } from "./app/components/Contact";
+import { Contact } from "./app/components/Contact";
+import { Footer } from "./app/components/Footer";
 
 export default function App() {
   return (
     <div className="min-h-screen">
+      <Nav />
+      {/* <main> */}
+      <Hero />
       <About />
       <Services />
       <FAQ />
       <Contact />
+      {/* </main> */}
+      <Footer />
     </div>
   )
 }
