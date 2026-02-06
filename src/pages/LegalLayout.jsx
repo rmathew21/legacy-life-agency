1242efdddc0298eed07e7b5860b1fd937a9c2b04
rob <robmathew21@gmail.com>
2026-02-05 23:24:50 -0600

    added PrivacyPolicy content

--- src/pages/LegalLayout.jsx
+++ src/pages/LegalLayout.jsx
@@ -4,7 +4,7 @@ export default function LegalLayout({ title, children }) {
     return (
         <div className="min-h-screen bg-white">
             <header className="border-b">
-                <div className="max-w-3xl mx-auto px-4 py-6">
+                <div className="max-w-4xl mx-auto px-6 py-24">
                     <Link to="/" className="text-sm underline">
                         Back to Home
                     </Link>
